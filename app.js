3cb9d627645599a3702620bce63aa9e6cc1931c7
yuno <hanganda23@naver.com>
2022-04-08 03:24:38 +0900

    feat: blocks

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext("2d");
+var ctx = canvas.getContext('2d');
 function Ball(x, y) {
     var _this = this;
     this.x = x;
@@ -9,10 +9,22 @@ function Ball(x, y) {
     this.ySpeed = 2.5;
     this.render = function () {
         ctx.beginPath();
-        ctx.fillStyle = 'skyblue';
+        ctx.fillStyle = '#8adffa';
         ctx.arc(_this.x, _this.y, _this.radius, 0, Math.PI * 2);
         ctx.fill();
         ctx.closePath();
+    };
+    this.turnLeft = function () {
+        _this.xSpeed = -Math.abs(_this.xSpeed);
+    };
+    this.turnRight = function () {
+        _this.xSpeed = Math.abs(_this.xSpeed);
+    };
+    this.turnDown = function () {
+        _this.ySpeed = Math.abs(_this.ySpeed);
+    };
+    this.turnUp = function () {
+        _this.ySpeed = -Math.abs(_this.ySpeed);
     };
     this.move = function (mx, my) {
         _this.x += mx;
@@ -27,7 +39,7 @@ function Paddle() {
     this.y = canvas.height - this.height - 10;
     this.render = function () {
         ctx.beginPath();
-        ctx.fillStyle = '#888';
+        ctx.fillStyle = '#666';
         ctx.rect(_this.x, _this.y, _this.width, _this.height);
         ctx.fill();
         ctx.closePath();
@@ -39,38 +51,107 @@ function Paddle() {
         _this.x = dx;
     };
 }
-function Block(x, y) {
-    this.width = 75;
-    this.height = 20;
-    this.x = x;
-    this.y = y;
-}
+var Visible;
+(function (Visible) {
+    Visible[Visible["SHOW"] = 0] = "SHOW";
+    Visible[Visible["HIDE"] = 1] = "HIDE";
+})(Visible || (Visible = {}));
+var Block = /** @class */ (function () {
+    function Block(x, y) {
+        var _this = this;
+        this.render = function () {
+            ctx.beginPath();
+            ctx.fillStyle = '#666';
+            ctx.rect(_this.x, _this.y, _this.width, _this.height);
+            ctx.fill();
+            ctx.closePath();
+        };
+        this.width = 75;
+        this.height = 20;
+        this.x = x;
+        this.y = y;
+        this.state = Visible.SHOW;
+    }
+    return Block;
+}());
 var ball = new Ball(canvas.width / 2, canvas.height / 2);
 var paddle = new Paddle();
 var gameover = function () {
     location.reload();
 };
+var makeBlock = function (row, col, padding, offsetLeft, offsetTop) {
+    var blocks = [];
+    for (var c = 0; c < col; c++) {
+        blocks[c] = [];
+        for (var r = 0; r < row; r++) {
+            var block = new Block(0, 0);
+            var x = offsetLeft + c * (block.width + padding);
+            var y = offsetTop + r * (block.height + padding);
+            block.x = x;
+            block.y = y;
+            blocks[c][r] = block;
+        }
+    }
+    return blocks;
+};
+var drawBlocks = function (blocks) {
+    for (var _i = 0, blocks_1 = blocks; _i < blocks_1.length; _i++) {
+        var col = blocks_1[_i];
+        for (var _a = 0, col_1 = col; _a < col_1.length; _a++) {
+            var block = col_1[_a];
+            if (block.state === Visible.SHOW) {
+                block.render();
+            }
+        }
+    }
+};
+var collisionDetection = function (blocks) {
+    for (var _i = 0, blocks_2 = blocks; _i < blocks_2.length; _i++) {
+        var col = blocks_2[_i];
+        for (var _a = 0, col_2 = col; _a < col_2.length; _a++) {
+            var block = col_2[_a];
+            var x = block.x, y = block.y, width = block.width, height = block.height;
+            var radius = ball.radius;
+            var isVisible = block.state === Visible.SHOW;
+            if (isVisible) {
+                var inX = x < ball.x + radius && x + width > ball.x - radius;
+                var inY = y < ball.y + radius && y + height > ball.y - radius;
+                if (inX && inY) {
+                    block.state = Visible.HIDE;
+                    ball.turnDown();
+                }
+            }
+        }
+    }
+};
+var blocks = makeBlock(4, 7, 15, 50, 40);
 var draw = function () {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // blocks
+    drawBlocks(blocks);
+    collisionDetection(blocks);
+    ball.render();
+    paddle.render();
     // ball
-    if (ball.x + ball.xSpeed < ball.radius || ball.x + ball.xSpeed > canvas.width - ball.radius) {
+    if (ball.x + ball.xSpeed < ball.radius ||
+        ball.x + ball.xSpeed > canvas.width - ball.radius) {
         ball.xSpeed = -ball.xSpeed;
-        // dx = -dx;
     }
     var xSpeed = ball.xSpeed, ySpeed = ball.ySpeed;
     var ballBottom = ball.y + ball.radius;
-    var isHit = (ballBottom + ySpeed === paddle.y) && ball.x > paddle.x && ball.x < paddle.x + paddle.width;
+    var isHit = ballBottom + ySpeed === paddle.y &&
+        ball.x > paddle.x &&
+        ball.x < paddle.x + paddle.width;
     if (ball.y + ySpeed < ball.radius) {
-        ball.ySpeed = -ySpeed;
-        // dy = -dy;
+        ball.turnDown();
     }
     else if (isHit) {
-        ball.ySpeed = -ySpeed;
+        ball.turnUp();
         if (control.leftPressed) {
-            ball.xSpeed = -(Math.abs(xSpeed));
+            ball.turnLeft();
         }
         else if (control.rightPressed) {
-            ball.xSpeed = Math.abs(xSpeed);
+            ball.turnRight();
         }
     }
     else if (ballBottom + ySpeed > canvas.height) {
@@ -85,9 +166,6 @@ var draw = function () {
     else if (control.rightPressed) {
         paddle.move(speed);
     }
-    // block
-    ball.render();
-    paddle.render();
 };
 var control = {
     leftPressed: false,
--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
-let canvas  = document.getElementById('myCanvas') as HTMLCanvasElement;
-const ctx = canvas.getContext("2d");
+let canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
 
 function Ball(x: number, y: number) {
   this.x = x;
@@ -8,34 +8,50 @@ function Ball(x: number, y: number) {
 
   this.xSpeed = 2.5;
   this.ySpeed = 2.5;
-  
+
   this.render = () => {
     ctx.beginPath();
-    ctx.fillStyle = 'skyblue';
+    ctx.fillStyle = '#8adffa';
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     ctx.fill();
     ctx.closePath();
-  }
+  };
 
-  this.move = (mx:number, my:number) => {
+  this.turnLeft = () => {
+    this.xSpeed = -Math.abs(this.xSpeed);
+  };
+
+  this.turnRight = () => {
+    this.xSpeed = Math.abs(this.xSpeed);
+  };
+
+  this.turnDown = () => {
+    this.ySpeed = Math.abs(this.ySpeed);
+  };
+
+  this.turnUp = () => {
+    this.ySpeed = -Math.abs(this.ySpeed);
+  };
+
+  this.move = (mx: number, my: number) => {
     this.x += mx;
     this.y += my;
-  }
+  };
 }
 
 function Paddle() {
   this.width = 120;
   this.height = 10;
-  this.x = (canvas.width - this.width) /2;
-  this.y = canvas.height - this.height - 10
+  this.x = (canvas.width - this.width) / 2;
+  this.y = canvas.height - this.height - 10;
 
   this.render = () => {
     ctx.beginPath();
-    ctx.fillStyle = '#888';
+    ctx.fillStyle = '#666';
     ctx.rect(this.x, this.y, this.width, this.height);
     ctx.fill();
     ctx.closePath();
-  }
+  };
 
   this.move = (mx: number) => {
     let dx = this.x + mx;
@@ -43,7 +59,12 @@ function Paddle() {
     dx = Math.min(dx, canvas.width - this.width);
 
     this.x = dx;
-  }
+  };
+}
+
+enum Visible {
+  SHOW,
+  HIDE,
 }
 
 class Block {
@@ -51,71 +72,126 @@ class Block {
   height: number;
   x: number;
   y: number;
+  state: Visible;
 
   constructor(x: number, y: number) {
     this.width = 75;
     this.height = 20;
     this.x = x;
     this.y = y;
+    this.state = Visible.SHOW;
   }
 
-   render = () => {
+  render = () => {
     ctx.beginPath();
-    ctx.fillStyle = '#888';
+    ctx.fillStyle = '#666';
     ctx.rect(this.x, this.y, this.width, this.height);
     ctx.fill();
     ctx.closePath();
-   }
+  };
 }
 
-const ball = new Ball(canvas.width/2, canvas.height/2);
+const ball = new Ball(canvas.width / 2, canvas.height / 2);
 const paddle = new Paddle();
 
 const gameover = () => {
   location.reload();
-}
+};
 
-const blockRow = 3;
-const blockCol = 5;
-const blockPadding = 10;
-const blockOffsetTop = 30;
-const blockOffsetLeft = 30;
+type Blocks = Block[][];
 
-const makeBlocks = () => {
-  const blocks: (Block[])[] = [];
+const makeBlock = (
+  row: number,
+  col: number,
+  padding: number,
+  offsetLeft: number,
+  offsetTop: number
+) => {
+  const blocks: Blocks = [];
 
-  for(let c=0; c<blockCol; c++) {
+  for (let c = 0; c < col; c++) {
     blocks[c] = [];
-    for(let r=0; r<blockRow; r++) {
-      blocks[c][r] = new Block(0,0);
+    for (let r = 0; r < row; r++) {
+      const block = new Block(0, 0);
+      const x = offsetLeft + c * (block.width + padding);
+      const y = offsetTop + r * (block.height + padding);
+      block.x = x;
+      block.y = y;
+      blocks[c][r] = block;
     }
   }
 
-}
+  return blocks;
+};
+
+const drawBlocks = (blocks: Blocks) => {
+  for (const col of blocks) {
+    for (const block of col) {
+      if (block.state === Visible.SHOW) {
+        block.render();
+      }
+    }
+  }
+};
+
+const collisionDetection = (blocks: Blocks) => {
+  for (const col of blocks) {
+    for (const block of col) {
+      const { x, y, width, height } = block;
+      const { radius } = ball;
+      const isVisible = block.state === Visible.SHOW;
+
+      if (isVisible) {
+        const inX = x < ball.x + radius && x + width > ball.x - radius;
+        const inY = y < ball.y + radius && y + height > ball.y - radius;
+
+        if (inX && inY) {
+          block.state = Visible.HIDE;
+          ball.turnDown();
+        }
+      }
+    }
+  }
+};
+
+const blocks = makeBlock(4, 7, 15, 50, 40);
 
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  // blocks
+  drawBlocks(blocks);
+  collisionDetection(blocks);
+
+  ball.render();
+  paddle.render();
+
   // ball
-  if (ball.x + ball.xSpeed < ball.radius || ball.x + ball.xSpeed > canvas.width - ball.radius) {
+  if (
+    ball.x + ball.xSpeed < ball.radius ||
+    ball.x + ball.xSpeed > canvas.width - ball.radius
+  ) {
     ball.xSpeed = -ball.xSpeed;
   }
 
-  const {xSpeed, ySpeed} = ball;
+  const { xSpeed, ySpeed } = ball;
   const ballBottom = ball.y + ball.radius;
-  const isHit = (ballBottom + ySpeed === paddle.y) && ball.x > paddle.x && ball.x < paddle.x + paddle.width
+  const isHit =
+    ballBottom + ySpeed === paddle.y &&
+    ball.x > paddle.x &&
+    ball.x < paddle.x + paddle.width;
 
-  if ( ball.y + ySpeed < ball.radius) {
-    ball.ySpeed = -ySpeed;
+  if (ball.y + ySpeed < ball.radius) {
+    ball.turnDown();
   } else if (isHit) {
-    ball.ySpeed = -ySpeed;
+    ball.turnUp();
 
     if (control.leftPressed) {
-      ball.xSpeed = -(Math.abs(xSpeed));
+      ball.turnLeft();
     } else if (control.rightPressed) {
-      ball.xSpeed = Math.abs(xSpeed);
+      ball.turnRight();
     }
-  } else if ( ballBottom + ySpeed > canvas.height) {
+  } else if (ballBottom + ySpeed > canvas.height) {
     gameover();
   }
 
@@ -128,23 +204,17 @@ const draw = () => {
   } else if (control.rightPressed) {
     paddle.move(speed);
   }
-
-  // block
-  
-
-  ball.render();
-  paddle.render();
-}
+};
 
 interface Control {
-  leftPressed: boolean,
-  rightPressed: boolean,
+  leftPressed: boolean;
+  rightPressed: boolean;
 }
 
 const control: Control = {
   leftPressed: false,
   rightPressed: false,
-}
+};
 
 const handleKeyDown = (e: KeyboardEvent) => {
   const { key } = e;
@@ -154,7 +224,7 @@ const handleKeyDown = (e: KeyboardEvent) => {
   } else if (key === 'ArrowRight') {
     control.rightPressed = true;
   }
-}
+};
 
 const handleKeyUp = (e: KeyboardEvent) => {
   const { key } = e;
@@ -164,7 +234,7 @@ const handleKeyUp = (e: KeyboardEvent) => {
   } else if (key === 'ArrowRight') {
     control.rightPressed = false;
   }
-}
+};
 
 document.addEventListener('keydown', handleKeyDown, false);
 document.addEventListener('keyup', handleKeyUp, false);
